5b6226a03a3a08eeed77ece4d81055ec53737af5
vishwasvishi <vishwasn200427@gmail.com>
2026-07-13 20:49:52 +0530

    Add package and java file

---
+++ oops-concepts/demo/src/inherit/tester.java
@@ -0,0 +1,10 @@
+package inherit;
+
+public class tester extends emp {
+
+	public tester(String ename, int eid, double sal) {
+		super(ename, eid, sal);
+		// TODO Auto-generated constructor stub
+	}
+
+}
