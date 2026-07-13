d29e71facd55ebfb0c8f682eb46d080caf059cee
vishwasvishi <vishwasn200427@gmail.com>
2026-07-13 20:53:18 +0530

    Add java file

---
+++ oops-concepts/demo/src/pack/Library.java
@@ -0,0 +1,16 @@
+package pack;
+
+public class Library {
+	static String libraryName = "lib";
+	String loc;
+	int bid;
+	public Library(String loc, int bid) {
+		this. loc= loc;
+		this.bid=bid;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
