bf7db1b48fc38e18a7956b23a7dda9a80eee3590
vishwasvishi <vishwasn200427@gmail.com>
2026-07-13 20:48:18 +0530

    Add package and java file

---
+++ oops-concepts/demo/src/inherit/emp_drive.java
@@ -0,0 +1,15 @@
+package inherit;
+
+public class emp_drive {
+
+	public static void main(String[] args) {
+		dev e1=new dev("abc",111,400000);
+		tester e2=new tester("anup",222,500000);
+		hr e3=new hr("pavan",333,500000);
+		e1.display();
+		e2.display();
+		e3.display();
+		
+	}
+
+}
