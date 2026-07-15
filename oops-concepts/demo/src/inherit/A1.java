fbc0032db3c6eab3d350ef200df356e644c79eef
vishwasvishi <vishwasn200427@gmail.com>
2026-07-15 21:06:05 +0530

    Add java file

---
+++ oops-concepts/demo/src/inherit/A1.java
@@ -0,0 +1,14 @@
+package inherit;
+
+public class A1 {
+	int x=100;
+	public void test1() {
+		System.out.println("From test1");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
