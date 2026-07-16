d8b49e041cb80052d980a30cc4df65effd602f72
vishwasvishi <vishwasn200427@gmail.com>
2026-07-16 22:44:51 +0530

    Add java file

---
+++ oops-concepts/demo/src/inherit/m7.java
@@ -0,0 +1,15 @@
+package inherit;
+
+public class m7 extends p6 {
+	public static void test() {
+		System.out.println("from test m7");
+	}
+
+	public static void main(String[] args) {
+		m7 ob=new m7();
+		ob.test();
+		
+
+	}
+
+}
