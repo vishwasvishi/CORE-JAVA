e92016c771c736164a9ad2915d79d99ff6bda476
vishwasvishi <vishwasn200427@gmail.com>
2026-07-30 23:26:30 +0530

    Add java file

---
+++ oops-concepts/demo/src/object_programes/p1.java
@@ -0,0 +1,18 @@
+package object_programes;
+
+public class p1 {
+	int a=100;
+	int b=200;
+
+	public static void main(String[] args) {
+		p1 ob1=new p1();
+		p1 ob2=new p1();
+		System.out.println(ob1);
+		System.out.println(ob1.toString());
+		System.out.println("---------------------");
+		System.out.println(ob2);
+		System.out.println(ob2.toString());
+
+	}
+
+}
