ab384ba7d72d7d1f46be331e1aa11e47d28c5ef6
vishwasvishi <vishwasn200427@gmail.com>
2026-08-05 22:32:03 +0530

    Add java file

---
+++ oops-concepts/demo/src/strings/p1.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class p1 {
+	public static void main(String[] args) {
+		String s1 = "vishi";
+		String s2 = "vishwas";
+		String s3 = "vishi";
+
+		System.out.println(s1==s2);
+		System.out.println(s1==s3);
+
+	}
+
+}
