2793707e53d727fb521544e1a881a372670b0afd
vishwasvishi <vishwasn200427@gmail.com>
2026-08-05 22:32:41 +0530

    Add java file

---
+++ oops-concepts/demo/src/strings/p3.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class p3 {
+	public static void main(String[] args) {
+		char[] ch = {'v','i','s','h','i'};
+		for(int i =0;i<ch.length;i++) {
+			System.out.println(ch[i]);
+		}
+		System.out.println("--------------------");
+		String s1 = new String(ch);
+		System.out.println(s1);
+	}
+	
+
+}
