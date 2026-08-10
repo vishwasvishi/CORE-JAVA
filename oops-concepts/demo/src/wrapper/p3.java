2a973a3caeb2dcc732818c7fb76331b690cba654
vishwasvishi <vishwasn200427@gmail.com>
2026-08-10 23:10:10 +0530

    Add java file

---
+++ oops-concepts/demo/src/wrapper/p3.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class p3 {
+	public static void test(int  num) {
+        System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+        Integer obj = new Integer(500);
+        test(obj);
+    }
+
+}
