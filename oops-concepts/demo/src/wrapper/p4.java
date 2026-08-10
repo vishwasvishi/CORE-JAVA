321e70e0df4d2f9b8c325c4cfe2b2657c0597fcd
vishwasvishi <vishwasn200427@gmail.com>
2026-08-10 23:10:31 +0530

    Add java file

---
+++ oops-concepts/demo/src/wrapper/p4.java
@@ -0,0 +1,14 @@
+package wrapper; 
+
+import java.util.ArrayList; 
+
+public class p4 {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(100);
+        a.add(200);
+        a.add(300);
+        System.out.println(a); 
+    }
+}
