e471f75ed21dcc2243d69be9529b43b369eaf1d1
vishwasvishi <vishwasn200427@gmail.com>
2026-08-10 12:46:21 +0530

    Add java package and file

---
+++ oops-concepts/demo/src/wrapper/p1.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+public class p1 {
+
+    
+    public static void test(Integer obj) {
+        System.out.println(obj);
+    }
+
+    public static void main(String[] args) {
+        int num = 500;
+        test(num);
+    }
+}
