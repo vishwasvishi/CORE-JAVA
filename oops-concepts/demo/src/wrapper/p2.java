d14fec38fa83eb3a98cffba29955e95efda633f4
vishwasvishi <vishwasn200427@gmail.com>
2026-08-10 23:09:49 +0530

    Add java file

---
+++ oops-concepts/demo/src/wrapper/p2.java
@@ -0,0 +1,12 @@
+package wrapper;
+
+public class p2 {
+    public static void main(String[] args) {
+        int num = 500;
+        
+        Integer obj = num;  
+        
+        System.out.println(num); 
+        System.out.println(obj); 
+    }
+}
