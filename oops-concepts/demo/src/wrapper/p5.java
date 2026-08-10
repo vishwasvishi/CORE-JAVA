388b4d3404228aa2429507b9a521e85bef4115b3
vishwasvishi <vishwasn200427@gmail.com>
2026-08-10 23:10:54 +0530

    Add java file

---
+++ oops-concepts/demo/src/wrapper/p5.java
@@ -0,0 +1,13 @@
+package wrapper;
+
+public class p5 {
+	 public static void main(String[] args) {
+		 Integer obj = new Integer(500);
+		 int num = obj;
+		 System.out.println(num); 
+		    System.out.println(obj);
+	}
+    
+   
+
+}
