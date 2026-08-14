9fcb567883ede36981a52227824f4de46be614c9
vishwasvishi <vishwasn200427@gmail.com>
2026-08-14 23:48:06 +0530

    Add update of m1 file

--- oops-concepts/demo/src/inherit/m1.java
+++ oops-concepts/demo/src/inherit/m1.java
@@ -1,6 +1,9 @@
 package inherit;
 
 public class m1 {
+	public static void test1() {
+    	System.out.println("daddy's home");
+    }
 	public void test() {
 		System.out.println("from test m1");
 	}
