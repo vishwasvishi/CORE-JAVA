46d03a6ecc54130a1a95a8426aa1495105c9a917
vishwasvishi <vishwasn200427@gmail.com>
2026-08-18 21:49:35 +0530

    Add p8 java file

---
+++ oops-concepts/demo/src/collection/p9.java
@@ -0,0 +1,16 @@
+package collection;
+import java.util.TreeSet;
+
+public class p9 {
+	public static void main(String[] args) {
+		TreeSet<String> s = new TreeSet<String>();
+
+        s.add("Disha");
+        s.add("Pooja");
+        s.add("Tamanna");
+        s.add("Anushka");
+        s.add("Rukmini");
+        System.out.println(s);
+	}
+
+}
